7a698b23adeb94458e80d0474bfa5c4b82c95493
Andy Huang <cerberus@ayxh.dev>
2024-02-27 22:10:55 +0000

    Remove solution

--- exercises/breakfast.js
+++ exercises/breakfast.js
@@ -1,26 +1,26 @@
 // Exercise 1, Part 1
 // Add the `async` keyword to the function and `await` to the `toast` method.
-export async function makeToast(bread, toaster) {
+export function makeToast(bread, toaster) {
     toaster.add(bread);
-    await toaster.toast();
+    toaster.toast();
     return toaster.pop();
 }
 
 // Exercise 1, Part 2
 // Add the `async` and `await` where needed to make the test pass.
 // The `grinder.grind()` and `coffeeMaker.brew()` methods are asynchronous.
-export async function makeCoffee(beans, grinder, coffeeMaker, cup) {
+export function makeCoffee(beans, grinder, coffeeMaker, cup) {
     grinder.add(beans);
-    await grinder.grind();
+    grinder.grind();
     coffeeMaker.add(grinder.dump());
-    await coffeeMaker.brew();
+    coffeeMaker.brew();
     return cup.fill(coffeeMaker.pour());
 }
 
 // Exercise 1, Part 3
 // Use the async `makeToast` and `makeCoffee` functions above to make breakfast.
-export async function makeBreakfast(bread, toaster, beans, grinder, coffeeMaker, cup) {
-    const toast = await makeToast(bread, toaster) // TODO: Use makeToast()
-    const coffee = await makeCoffee(beans, grinder, coffeeMaker, cup) // TODO: Use makeCoffee()
+export function makeBreakfast(bread, toaster, beans, grinder, coffeeMaker, cup) {
+    const toast = null // TODO: Use makeToast()
+    const coffee = null // TODO: Use makeCoffee()
     return { toast, coffee }
 }
