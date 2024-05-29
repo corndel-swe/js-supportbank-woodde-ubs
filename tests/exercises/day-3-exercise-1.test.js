d6c2a72fd733ae6ada816301e369e0534a699154
Simon Haines <nimon@hotmail.co.uk>
2024-05-29 15:31:29 +0100

    edit test

--- tests/exercises/day-3-exercise-1.test.js
+++ tests/exercises/day-3-exercise-1.test.js
@@ -3,16 +3,16 @@ import { existsSync } from 'fs'
 import 'dotenv/config'
 
 describe('Dotenv', () => {
-    it('should have a .env file in the root', () => {
-        const envPath = './.env'
-        const envExists = existsSync(envPath)
-        assert.ok(envExists, '.env file not found')
-    })
+  it('should have a .env file in the root', () => {
+    const envPath = './.env'
+    const envExists = existsSync(envPath)
+    assert.ok(envExists, '.env file not found')
+  })
 })
 
 describe('Environment Variables', () => {
-    it('should have a POKEAPI_URL environment variable', () => {
-        const pokeapi = process.env.POKEAPI_URL
-        assert.ok(pokeapi, 'POKEAPI_URL environment variable not found')
-    })
+  it('should have a SECRET_KEY environment variable', () => {
+    const pokeapi = process.env.SECRET_KEY
+    assert.ok(pokeapi, 'SECRET_KEY environment variable not found')
+  })
 })
