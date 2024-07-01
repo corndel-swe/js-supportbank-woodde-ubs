0c935d7d7a35ea4d41b5bd34f044be152e500f7d
Denise Wood <deniseofarrell@gmail.com>
2024-07-01 12:08:42 +0100

    committing day 1 - ex3

--- cli/transaction.js
+++ cli/transaction.js
@@ -1,4 +1,5 @@
 import { Command } from 'commander'
+import Transaction from '../models/Transaction.js'
 
 const transactionController = new Command('transaction')
 
@@ -7,6 +8,8 @@ transactionController
   .description('Log transaction data to the console')
   .action((from, to, amount) => {
     // TODO
+    const transaction = new Transaction(from, parseInt(amount))
+    console.log(transaction)
   })
 
 export default transactionController
