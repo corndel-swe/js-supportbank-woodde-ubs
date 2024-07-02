88bf6504833f76e27173c91d705b5b73c8b689d7
Denise Wood <deniseofarrell@gmail.com>
2024-07-02 14:22:48 +0100

    committing day 2 - ex2 & 3

--- exercises/fileIO.js
+++ exercises/fileIO.js
@@ -6,11 +6,16 @@ export async function readHelloWorld() {
   const path = new URL('./resources/1-hello-world.txt', import.meta.url)
 
   // TODO: Return the contents of the file as a utf-8 string
+  const content = await fs.readFile(path, 'utf-8')
+  return content
 }
 
 // Part 2: Return the contents of ./resources/2-diary-entry.txt
 export async function readDiaryEntry() {
   // TODO: Return the contents of the file as a utf-8 string
+  const path = new URL('./resources/2-diary-entry.txt', import.meta.url)
+  const content = await fs.readFile(path, 'utf-8')
+  return content
 }
 
 // Part 3: Overwrite ./resources/3-save-data.txt with saveDataString
@@ -22,12 +27,14 @@ export async function writeSaveData(saveData) {
   const saveDataString = JSON.stringify(saveData)
 
   // TODO: Write the saveData string to the file
+  await fs.writeFile(path, saveDataString)
 }
 
 // Part 4: Append text to ./resources/4-log-file.txt
 export async function appendLogEntry(logEntry) {
   // The text to append is given:
   const textToAppend = logEntry + '\n'
-
+  const path = new URL('./resources/4-log-file.txt', import.meta.url)
   // TODO: Append the text to the file
+  await fs.appendFile(path, textToAppend)
 }
--- exercises/validateId.js
+++ exercises/validateId.js
@@ -9,16 +9,26 @@ export function validateId(id) {
   // The id should be a string:
   if (typeof id !== 'string') {
     // TODO: Add your code to throw an error here
+    throw new Error('ID should be a string')
   }
 
   // The id should be an odd number of characters long:
   // TODO: throw an error on this condition
+  if (id.length % 2 == 0) {
+    throw new Error('ID should be an odd number')
+  }
 
   // The id should contain the letter 'a':
   // TODO: throw an error on this condition
+  if (!id.includes('a')) {
+    throw new Error('ID should contain a')
+  }
 
   // The id should be all lowercase:
   // TODO: throw an error on this condition
+  if (id !== id.toLowerCase()) {
+    throw new Error('ID should be lowercase')
+  }
 }
 
 // Exercise 2 Part 2
@@ -26,6 +36,11 @@ export function validateId(id) {
 // Return `true` if the id is valid.
 // If an error is thrown: catch it, log a useful message, then return `false`
 export function isIdValid(id, logger) {
-  logger.error('ID is invalid')
-  return null
+  try {
+    validateId(id)
+  } catch (error) {
+    logger.error('ID is invalid')
+    return false
+  }
+  return true
 }
